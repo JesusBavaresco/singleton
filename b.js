f5dcfe32b84fe34b291fbcc4a513f4fa38b59836
Ian Zamora <ianzamora@MacBook-Pro-de-Ian.local>
2021-07-19 21:20:28 -0600

    ready

---
+++ b.js
@@ -0,0 +1,10 @@
+import logger from './logger.js'
+
+function secondRun() {
+    logger.printCount();
+    logger.log('Lorem ipsum dolor sit amen...');
+    logger.printCount();
+}
+export {
+    secondRun
+};
